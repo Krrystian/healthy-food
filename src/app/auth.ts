32d97b51ba41713402d47bc26b50ef5c9d40fed3
Krystian Cichorz <krystiancichorz708@gmail.com>
2024-06-24 15:35:57 +0200

    Database add from google provider - adapter

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -4,8 +4,10 @@ import { signInSchema } from "./lib/zod"
 import bcrypt from "bcryptjs"
 import prisma from "./lib/prisma"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID,
