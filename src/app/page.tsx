59d569a8ee694c2866c92526264149c14f91716f
Krystian Cichorz <krystiancichorz708@gmail.com>
2024-06-29 00:12:45 +0200

    Lenis smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
+"use client";
 import React from "react";
 import { auth } from "./auth";
-import { SignInButton } from "./components/SignInOut/SignInButton";
-import { SignOutButton } from "./components/SignInOut/SignOutButton";
-import Link from "next/link";
+import SmoothScroll from "./hooks/useSmoothScroll";
 
-export default async function Home() {
-  let session = await auth();
+export default function Home() {
+  const getSession = async () => {
+    let session = await auth();
+  };
+  SmoothScroll();
   return (
-    <main className="flex flex-col gap-4 min-h-[200vh] bg-[#023047]">
-      {/* Hello {session ? session.user?.name : "Guest"} */}
-      {/* <SignInButton />
-      <SignOutButton /> */}
+    <main className="flex flex-col gap-4 min-h-[200vh] bg-[#023047] justify-center items-center">
+      <p className="text-7xl text-white">TEST TEST TEST TEST</p>
     </main>
   );
 }
