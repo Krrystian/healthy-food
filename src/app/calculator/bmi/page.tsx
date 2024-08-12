61d8c2cc7e12e3fa3c2b57410fd560448516bae1
Dominika Dobosz <dominika.dobosz23@gmail.com>
2024-08-12 15:56:10 +0200

    init

---
+++ src/app/calculator/bmi/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from 'react'
+
+export default function Page() {
+    return (
+    <div className='flex w-screen h-screen justify-center items-center'>
+        <h2 className='text-7xl text-green-300'>Hello</h2>
+    </div>
+  )
+}
+
